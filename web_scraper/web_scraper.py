395edb5ff4e4b854e3877b585b58aeb4f1b72a7d
yongjip <yongjip@coupang.com>
2019-02-06 17:19:49 +0900

    web scraper -- test #2

--- web_scraper/web_scraper.py
+++ web_scraper/web_scraper.py
@@ -5,20 +5,11 @@ import time
 import re
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from time import sleep
 
 
 url = 'https://kr.iherb.com/r/a/71684'
-# headers = {'Content-Type': 'application/json; charset=utf-8',
-#             'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36'}
 
-# headers = {'Content-Type': 'application/json; charset=utf-8',
-#             'User-Agent' : 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Mobile/15C153'}
-
-# r = requests.get(url, headers=headers)
-# content = r.content
-
-# with open('test.html', 'wb') as f:
-#     f.write(content)
 
 CHROMEDRIVER_PATH = "./chromedriver"
 
@@ -32,27 +23,55 @@ options.add_argument("--disable-extensions")
 
 driver = webdriver.Chrome(CHROMEDRIVER_PATH, options=options)
 
-driver.get("https://kr.iherb.com/r/a/62121")
+driver.get(url)
 
-class_names = ['review-headline', 'posted-date', 'flavor', 'review-text']
+sleep(4)
+html = driver.execute_script("return document.documentElement.outerHTML")
 
-
-class_names = 'review-text line-clamp'
-elems = driver.find_elements_by_class_name(class_names)
-
-for elem_set in elems:
-    print(elem_set.text)
-    print()
-
-elements = []
-for class_names in class_names:
-    elements.append(driver.find_elements_by_class_name(class_names))
+soup = BeautifulSoup(html, 'html.parser')
 
 
-for elem_set in elements[3]:
-    print(elem_set.text)
-    print()
+reviews = soup.findAll("div", {"class": "review-core-section"})
+print(len(reviews))
+
+start_pat = r'<svg class="icon icon-stars_(\d)0 stars-rating">'
+for review_soup in reviews:
+    stars = review_soup.find('svg',  {'class': 'icon'})
+    m = re.search(start_pat, str(stars))
+    stars = m.group(1) if m else None
+    print(stars)
+    headline = review_soup.find('h3', {'class': 'review-headline'}).text
+    posted_date = review_soup.find('span', {'class': 'posted-date'}).text
+    posted_date = review_soup.find('span', {'class': 'posted-date'}).text
+    review_body = review_soup.find('div', {'class': 'review-text'}).text
 
 
-content = driver.page_source
+print(headline, posted_date, stars, review_body, sep='\n')
 
+review_soup.find('use')
+
+
+paging = soup.find("div", {"class": "paging"})
+current_page_num = paging.find('button', {'class': 'selected-page'}).text
+
+driver.find_elements_by_class_name('button')
+
+page = driver.find_elements_by_class_name('page')
+driver.find_elements_by_class_name('selected-page')
+next_page = driver.find_elements_by_class_name('arrow-button')[1]
+
+
+page.click()
+
+
+# headers = {'Content-Type': 'application/json; charset=utf-8',
+#             'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36'}
+
+# headers = {'Content-Type': 'application/json; charset=utf-8',
+#             'User-Agent' : 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_2_1 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Mobile/15C153'}
+
+# r = requests.get(url, headers=headers)
+# content = r.content
+
+# with open('test.html', 'wb') as f:
+#     f.write(content)
