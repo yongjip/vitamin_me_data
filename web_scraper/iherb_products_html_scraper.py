8f1d4a644e412473bd9ff531ef5feea67141fbba
yongjip <yongjip@coupang.com>
2020-01-31 17:03:19 +0900

    some pages don't show at all. put try & except

--- web_scraper/iherb_products_html_scraper.py
+++ web_scraper/iherb_products_html_scraper.py
@@ -18,7 +18,7 @@ start_product_id = 1
 end_product_id = 10000
 
 output_dir = '../data/products_html_pages/'
-output_json_name = '../products_html_pages_{start_id}_{end_id}.json'
+output_json_name = 'products_html_pages_{start_id}_{end_id}.json'
 CHROMEDRIVER_PATH = "./chromedriver"
 
 os.makedirs(output_dir, exist_ok=True)
@@ -40,26 +40,31 @@ product_raw_html_dict = {}
 
 for product_id in tqdm.tqdm(range(start_product_id, end_product_id+1)):
     url = f'https://iherb.com/pr/a/{product_id}'
-    driver.get(url)
-    change_default_country(driver, 'US')
-    html = driver.execute_script("return document.documentElement.outerHTML")
-    page = WebPage(html)
-    page.clean_source()
-    page.remove_excess_whitespace()
-    minified_html = page.get_source()
-    product_raw_html_dict[product_id] = minified_html
+    try:
+        driver.get(url)
+        change_default_country(driver, 'US')
+        html = driver.execute_script("return document.documentElement.outerHTML")
+        page = WebPage(html)
+        page.clean_source()
+        page.remove_excess_whitespace()
+        minified_html = page.get_source()
+        product_raw_html_dict[product_id] = minified_html
 
-    if product_id % size == 0 or end_product_id == product_id:
-        if product_id % size == 0:
-            product_id_starting_from = product_id - size + 1
-        else:
-            num_processed = product_id % size - 1
-            product_id_starting_from = product_id - num_processed
-        output_destination = output_dir + output_json_name.format(start_id=product_id_starting_from, end_id=product_id)
-        current_time = datetime.now()
-        # print(f'[{current_time}]Dumping: {output_destination}', sep='\n')
-        sleep(0.2)
-        logger.info(f'Dumping: {output_destination}')
-        with open(output_destination, 'w') as f:
-            json.dump(product_raw_html_dict, f)
-        product_raw_html_dict = {}
+    except:
+        pass
+
+    finally:
+        if product_id % size == 0 or end_product_id == product_id:
+            if product_id % size == 0:
+                product_id_starting_from = product_id - size + 1
+            else:
+                num_processed = product_id % size - 1
+                product_id_starting_from = product_id - num_processed
+            output_destination = output_dir + output_json_name.format(start_id=product_id_starting_from, end_id=product_id)
+            current_time = datetime.now()
+            # print(f'[{current_time}]Dumping: {output_destination}', sep='\n')
+            sleep(0.2)
+            logger.info(f'Dumping: {output_destination}')
+            with open(output_destination, 'w') as f:
+                json.dump(product_raw_html_dict, f)
+            product_raw_html_dict = {}
